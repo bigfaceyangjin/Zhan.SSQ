aeeedf515f1fb0fa625f03a4ceedf6dc8c0bed5c
yangjin <2822102009@qq.com>
2022-06-12 22:49:06 +0800

    基本完成

---
+++ Zhan.SSQ/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace Zhan.SSQ
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.lbl_blue = new System.Windows.Forms.Label();
+			this.lbl_red6 = new System.Windows.Forms.Label();
+			this.lbl_red5 = new System.Windows.Forms.Label();
+			this.lbl_red4 = new System.Windows.Forms.Label();
+			this.lbl_red3 = new System.Windows.Forms.Label();
+			this.lbl_red2 = new System.Windows.Forms.Label();
+			this.lbl_red1 = new System.Windows.Forms.Label();
+			this.btn_stop = new System.Windows.Forms.Button();
+			this.btn_start = new System.Windows.Forms.Button();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.lbl_blue);
+			this.groupBox1.Controls.Add(this.lbl_red6);
+			this.groupBox1.Controls.Add(this.lbl_red5);
+			this.groupBox1.Controls.Add(this.lbl_red4);
+			this.groupBox1.Controls.Add(this.lbl_red3);
+			this.groupBox1.Controls.Add(this.lbl_red2);
+			this.groupBox1.Controls.Add(this.lbl_red1);
+			this.groupBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.groupBox1.Location = new System.Drawing.Point(78, 59);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(922, 310);
+			this.groupBox1.TabIndex = 0;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "双色球结果区";
+			// 
+			// lbl_blue
+			// 
+			this.lbl_blue.AutoSize = true;
+			this.lbl_blue.Font = new System.Drawing.Font("Microsoft YaHei UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.lbl_blue.ForeColor = System.Drawing.Color.DodgerBlue;
+			this.lbl_blue.Location = new System.Drawing.Point(743, 154);
+			this.lbl_blue.Name = "lbl_blue";
+			this.lbl_blue.Size = new System.Drawing.Size(56, 41);
+			this.lbl_blue.TabIndex = 6;
+			this.lbl_blue.Text = "00";
+			// 
+			// lbl_red6
+			// 
+			this.lbl_red6.AutoSize = true;
+			this.lbl_red6.Font = new System.Drawing.Font("Microsoft YaHei UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.lbl_red6.ForeColor = System.Drawing.Color.Red;
+			this.lbl_red6.Location = new System.Drawing.Point(545, 154);
+			this.lbl_red6.Name = "lbl_red6";
+			this.lbl_red6.Size = new System.Drawing.Size(56, 41);
+			this.lbl_red6.TabIndex = 5;
+			this.lbl_red6.Text = "00";
+			// 
+			// lbl_red5
+			// 
+			this.lbl_red5.AutoSize = true;
+			this.lbl_red5.Font = new System.Drawing.Font("Microsoft YaHei UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.lbl_red5.ForeColor = System.Drawing.Color.Red;
+			this.lbl_red5.Location = new System.Drawing.Point(438, 154);
+			this.lbl_red5.Name = "lbl_red5";
+			this.lbl_red5.Size = new System.Drawing.Size(56, 41);
+			this.lbl_red5.TabIndex = 4;
+			this.lbl_red5.Text = "00";
+			// 
+			// lbl_red4
+			// 
+			this.lbl_red4.AutoSize = true;
+			this.lbl_red4.Font = new System.Drawing.Font("Microsoft YaHei UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.lbl_red4.ForeColor = System.Drawing.Color.Red;
+			this.lbl_red4.Location = new System.Drawing.Point(333, 154);
+			this.lbl_red4.Name = "lbl_red4";
+			this.lbl_red4.Size = new System.Drawing.Size(56, 41);
+			this.lbl_red4.TabIndex = 3;
+			this.lbl_red4.Text = "00";
+			// 
+			// lbl_red3
+			// 
+			this.lbl_red3.AutoSize = true;
+			this.lbl_red3.Font = new System.Drawing.Font("Microsoft YaHei UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.lbl_red3.ForeColor = System.Drawing.Color.Red;
+			this.lbl_red3.Location = new System.Drawing.Point(237, 154);
+			this.lbl_red3.Name = "lbl_red3";
+			this.lbl_red3.Size = new System.Drawing.Size(56, 41);
+			this.lbl_red3.TabIndex = 2;
+			this.lbl_red3.Text = "00";
+			// 
+			// lbl_red2
+			// 
+			this.lbl_red2.AutoSize = true;
+			this.lbl_red2.Font = new System.Drawing.Font("Microsoft YaHei UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.lbl_red2.ForeColor = System.Drawing.Color.Red;
+			this.lbl_red2.Location = new System.Drawing.Point(135, 154);
+			this.lbl_red2.Name = "lbl_red2";
+			this.lbl_red2.Size = new System.Drawing.Size(56, 41);
+			this.lbl_red2.TabIndex = 1;
+			this.lbl_red2.Text = "00";
+			// 
+			// lbl_red1
+			// 
+			this.lbl_red1.AutoSize = true;
+			this.lbl_red1.Font = new System.Drawing.Font("Microsoft YaHei UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.lbl_red1.ForeColor = System.Drawing.Color.Red;
+			this.lbl_red1.Location = new System.Drawing.Point(38, 154);
+			this.lbl_red1.Name = "lbl_red1";
+			this.lbl_red1.Size = new System.Drawing.Size(56, 41);
+			this.lbl_red1.TabIndex = 0;
+			this.lbl_red1.Text = "00";
+			// 
+			// btn_stop
+			// 
+			this.btn_stop.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.btn_stop.Location = new System.Drawing.Point(648, 428);
+			this.btn_stop.Name = "btn_stop";
+			this.btn_stop.Size = new System.Drawing.Size(158, 41);
+			this.btn_stop.TabIndex = 7;
+			this.btn_stop.Text = "Stop";
+			this.btn_stop.UseVisualStyleBackColor = true;
+			this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+			// 
+			// btn_start
+			// 
+			this.btn_start.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.btn_start.Location = new System.Drawing.Point(233, 428);
+			this.btn_start.Name = "btn_start";
+			this.btn_start.Size = new System.Drawing.Size(158, 41);
+			this.btn_start.TabIndex = 1;
+			this.btn_start.Text = "Start";
+			this.btn_start.UseVisualStyleBackColor = true;
+			this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1075, 544);
+			this.Controls.Add(this.btn_stop);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.btn_start);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "FrmSSQ";
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Label lbl_red1;
+		private System.Windows.Forms.Label lbl_blue;
+		private System.Windows.Forms.Label lbl_red6;
+		private System.Windows.Forms.Label lbl_red5;
+		private System.Windows.Forms.Label lbl_red4;
+		private System.Windows.Forms.Label lbl_red3;
+		private System.Windows.Forms.Label lbl_red2;
+		private System.Windows.Forms.Button btn_stop;
+		private System.Windows.Forms.Button btn_start;
+	}
+}
+
---
+++ Zhan.SSQ/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zhan.SSQ
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private string[] blueStrs = {"01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15","16" };
+		private string[] redStrs = {"01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15","16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32","33" };
+		private static readonly object SSQ = new object();
+		/// <summary>
+		/// 项目启动
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void btn_start_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				this.btn_start.Text = "运行ing...";
+				this.btn_start.Enabled = false;
+				this.lbl_red1.Text = "00";
+				this.lbl_red2.Text = "00";
+				this.lbl_red3.Text = "00";
+				this.lbl_red4.Text = "00";
+				this.lbl_red5.Text = "00";
+				this.lbl_red6.Text = "00";
+				this.lbl_blue.Text = "00";
+				TaskFactory taskFactory = new TaskFactory();
+				//不允许出现重复数字 
+				foreach (Control control in this.groupBox1.Controls)
+				{
+					if (control is Label)
+					{
+						Label lbl = (Label)control;
+						if (control.Name.Contains("blue"))//蓝球
+						{
+							taskFactory.StartNew(() =>
+							{
+								while (true)
+								{
+									//获取号码下标
+									int blueIndex = this.GetRandomLong(0, blueStrs.Length);
+									//通过下标访问蓝球集合
+									this.UpdateControl(lbl, this.blueStrs[blueIndex]);
+								}
+							});
+						}
+						//红球
+						else {
+							taskFactory.StartNew(()=> {
+								while (true) {
+									int redIndex = this.GetRandomLong(0, this.redStrs.Length);
+									lock (SSQ)
+									{
+										//遍历其他五个球 如果有相同存在 就进入下一次循环
+										if (this.IsExist(redStrs[redIndex]))
+										{
+											continue; 
+										}
+									}
+									this.UpdateControl(lbl, this.redStrs[redIndex]);
+								} 
+							});
+						}
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"双色球项目出现异常：{ex.Message}");
+			}
+		}
+
+		/// <summary>
+		/// 获取随机数
+		/// </summary>
+		/// <param name="min"></param>
+		/// <param name="max"></param>
+		/// <returns></returns>
+		public int GetRandom(int min,int max) {
+			Guid guid= Guid.NewGuid();//全球唯一
+			string guidStr = guid.ToString();
+			int Need = DateTime.Now.Millisecond;
+			for (int i = 0; i < guidStr.Length; i++)
+			{
+				switch (guidStr[i]) {
+					case 'a':
+					case 'b':
+					case 'c':
+					case 'd':
+					case 'e':
+					case 'f':
+					case 'g':
+						Need += 1;
+						break;
+					case 'h':
+					case 'i':
+					case 'j':
+					case 'k':
+					case 'l':
+					case 'm':
+					case 'n':
+						Need += 2;
+						break;
+					case 'o':
+					case 'p':
+					case 'q':
+						Need += 3;
+						break;
+					case 'r':
+					case 's':
+					case 't':
+					case 'u':
+					case 'v':
+					case 'w':
+					case 'x':
+					case 'y':
+					case 'z':  
+						Need += 4;
+						break;
+					default:
+						Need += 5;
+						break;
+				}
+			} 
+			return new Random(Need).Next(min, max);
+		}
+
+		/// <summary>
+		/// 将获取随机数延时
+		/// </summary>
+		/// <param name="min"></param>
+		/// <param name="max"></param>
+		/// <returns></returns>
+		public int GetRandomLong(int min,int max) {
+			Thread.Sleep(this.GetRandom(500,1000));
+			return this.GetRandom(min,max);
+		}
+		/// <summary>
+		/// 解决异步线程修改UI线程问题
+		/// </summary>
+		/// <param name="lbl"></param>
+		/// <param name="name"></param>
+		public void UpdateControl(Label lbl, string name)
+		{
+			if (lbl.InvokeRequired)
+			{
+				lbl.Invoke(new Action(() =>
+				{
+					lbl.Text = name;
+				}));
+			}
+			else {
+				lbl.Text = name;
+			}
+		}
+
+		/// <summary>
+		/// 判断是否存在重复球号
+		/// </summary>
+		/// <param name="name"></param>
+		/// <returns></returns>
+		public bool IsExist(string name) {
+			foreach (Control control in this.groupBox1.Controls)
+			{ 
+				if (control is Label)
+				{
+					if (control.Text == name)
+					{
+						return true;//存在重复球号
+					}
+				}
+			}
+			return false;
+		}
+		private void btn_stop_Click(object sender, EventArgs e)
+		{
+			CancellationTokenSource cls = new CancellationTokenSource();
+			cls.Cancel();
+		}
+	}
+}
